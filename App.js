216ed3285837a7bc9295eee9191d48f66c0f9115
jfmam <49827304+jfmam@users.noreply.github.com>
2019-12-14 23:37:20 +0900

    remove nativebase

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Container, Text } from 'native-base';
+
 import SwitchNavigator from './navigation/switchNavigator'
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import { Provider } from "react-redux";
 import {createStore} from 'redux'
-
 import axios from 'axios'
 
 
@@ -17,29 +16,12 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      isReady: false,
-    };
   }
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
   render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
+   
   return (
-    
-    <Container>
      <SwitchNavigator/>
-    </Container>
- 
   );
   }
 }
