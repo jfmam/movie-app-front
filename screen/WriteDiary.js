faa21bc6263014960ef0fcadcbff8ed0b7698b12
jfmam <49827304+jfmam@users.noreply.github.com>
2019-12-23 23:19:03 +0900

    imagePicker

--- screen/WriteDiary.js
+++ screen/WriteDiary.js
@@ -1,9 +1,13 @@
 import {View, SafeAreaView, ImageBackground,StyleSheet,Platform,Text,Image} from 'react-native'
-import {ScrollView} from 'react-native-gesture-handler'
+import {ScrollView, TouchableOpacity, TextInput} from 'react-native-gesture-handler'
 import React,{ useState, useEffect } from 'react'
 import {Rating} from 'react-native-elements'
 
 export default WriteDiary=(props)=>{
+    const [memo,setMemo]=useState('')
+    const imagePicker=()=>{
+
+    }
     return(
         <SafeAreaView style={styles.container}>    
             {/* < ImageBackground style={{width:100}} source = 'https://lorempixel.com/200/200/animals' / >
@@ -11,15 +15,27 @@ export default WriteDiary=(props)=>{
             <Text>영화제목</Text>
             <Text>영화 정보</Text>
         <ScrollView>
-        
+        <View>
+        <Text>Rating</Text>
         <Rating
              type='star'
              ratingCount={5}
              imageSize={18}
              onFinishRating={this.ratingCompleted}
         />
+        </View>
+        <View>
+            <Text>Date</Text>
+            <Text>____________</Text>
+        </View>
+         <View>
+            {/* <TouchableOpacity><Image source=""/></TouchableOpacity> */}
+            <TouchableOpacity onPress={()=>{imagePicker()}}><Text>Add Photo</Text></TouchableOpacity>
+        </View>
+        <TextInput multiline numberOfLines={10} onChangeText={(text)=>setMemo(Text)}/>
         </ScrollView>
-        <Text>Date</Text>
+      
+
         </SafeAreaView> 
     )
 }
